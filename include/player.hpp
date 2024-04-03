45fc44a731391b9c3b48a0cf083e228e9b76dba5
Makoto Powers <makotopowers15@icloud.com>
2024-04-02 23:42:51 -0400

    functionality for evaluating a current hand. No jokers implemented yet. Add card function. Tests added.

--- include/player.hpp
+++ include/player.hpp
@@ -2,15 +2,52 @@
 #define PLAYER_HPP
 
 #include <iostream>
+#include <queue>
 #include <string>
 #include <vector>
-#include <memory>
 
-class Player
-{
-public:
-    Player();
-    ~Player();
+enum class Suit { C, D, H, S };
+
+struct Hand {
+  std::vector<std::vector<bool>> cards;
+  std::vector<bool> cardsC;
+  std::vector<bool> cardsD;
+  std::vector<bool> cardsH;
+  std::vector<bool> cardsS;
+
+  std::priority_queue<int, std::vector<int>, std::greater<int>> highCards;
+  int pair = 0;
+  int twoPair = 0;
+  int trips = 0;
+  int straight_ = 0;
+  int flush = 0;
+  int fullHouse = 0;
+  int quads = 0;
+  int straightFlush = 0;
+  int quint = 0;
+
+  int score;
+
+  Hand() {
+    std::cout << "Hand constructor called" << std::endl;
+    cardsC = std::vector<bool>(14, false);
+    cardsD = std::vector<bool>(14, false);
+    cardsH = std::vector<bool>(14, false);
+    cardsS = std::vector<bool>(14, false);
+    cards = {cardsC, cardsD, cardsH, cardsS};
+    score = 0;
+  }
 };
 
-#endif // PLAYER_HPP
+class Player {
+ public:
+  Player();
+  ~Player();
+  void addCard(int card);
+  void addCard(std::string card);
+  Hand *hand;
+  void evaluateHand(Hand *hand);
+  void printHand();
+};
+
+#endif  // PLAYER_HPP
--- src/player.cpp
+++ src/player.cpp
@@ -1,11 +1,115 @@
 #include "player.hpp"
 
-Player::Player()
-{
-    std::cout << "Player constructor called" << std::endl;
+#include <queue>
+#include <vector>
+
+Player::Player() { std::cout << "Player constructor called" << std::endl; }
+
+Player::~Player() { std::cout << "Player destructor called" << std::endl; }
+
+void Player::evaluateHand(Hand *hand) {
+  std::priority_queue<int, std::vector<int>, std::greater<int>> highCards;
+  int pair = 0;
+  int twoPair = 0;
+  int trips = 0;
+  int straight_ = 0;
+  int flush = 0;
+  int fullHouse = 0;
+  int quads = 0;
+  int straightFlush = 0;
+  int quint = 0;
+
+  std::queue<int> straight;
+  std::vector<std::priority_queue<int, std::vector<int>, std::greater<int>>>
+      flushes;
+  std::priority_queue<int, std::vector<int>, std::greater<int>> flushC;
+  std::priority_queue<int, std::vector<int>, std::greater<int>> flushD;
+  std::priority_queue<int, std::vector<int>, std::greater<int>> flushH;
+  std::priority_queue<int, std::vector<int>, std::greater<int>> flushS;
+  flushes = {flushC, flushD, flushH, flushS};
+
+  for (int i = 0; i < hand->cards[0].size(); i++) {
+    int count = 0;
+    for (int j = 0; j < hand->cards.size(); j++) {
+      if (hand->cards[j][i]) {
+        count++;
+        flushes[j].push(i + 1);
+        if (flushes[j].size() > 5) {
+          flushes[j].pop();
+        }
+      }
+    }
+    switch (count) {
+      case 0:
+        continue;
+      case 4:
+        quads = i + 1;
+        break;
+      case 3:
+        if (trips > 0 && trips > pair) {
+          pair = trips;
+          trips = i + 1;
+          fullHouse = trips;
+        } else if (pair > 0) {
+          trips = i + 1;
+          fullHouse = i + 1;
+        } else {
+          trips = i + 1;
+        }
+        break;
+      case 2:
+        if (pair > 0) {
+          twoPair = i + 1;
+        } else {
+          pair = i + 1;
+        }
+        break;
+      default:
+        highCards.push(i + 1);
+        break;
+    }
+
+    if (count > 0) {
+      if (straight.empty()) {
+        straight.push(i + 1);
+      } else if (straight.back() == i) {
+        straight.push(i + 1);
+      } else {
+        straight = std::queue<int>();
+        straight.push(i + 1);
+      }
+      if (straight.size() > 5) straight.pop();
+    }
+    if (straight.size() > 4) straight_ = straight.back();
+
+    for (int j = 0; j < hand->cards.size(); j++) {
+      if (flushes[j].size() > 4) {
+        flush = flushes[j].top() + 4;
+        if (straight_ == flush) {
+          straightFlush = flush;
+        }
+      }
+    }
+  }
+
+  hand->flush = flush;
+  hand->fullHouse = fullHouse;
+  hand->quads = quads;
+  hand->pair = pair;
+  hand->straight_ = straight_;
+  hand->straightFlush = straightFlush;
+  hand->trips = trips;
+  hand->twoPair = twoPair;
+  hand->quint = quint;
 }
 
-Player::~Player()
-{
-    std::cout << "Player destructor called" << std::endl;
+void Player::addCard(int card) {
+  int suit = card / 13;
+  int value = card % 13;
+  if (value == 0) {
+    hand->cards[suit][0] = true;
+    hand->cards[suit][13] = true;
+  } else {
+    hand->cards[suit][value] = true;
+  }
 }
